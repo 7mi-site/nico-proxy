d6cbdd30fa0520cbd9038523a63f0e375c05e8b1
ちのなみ <tino_nanami@7mi.site>
2023-07-29 10:30:33 +0900

    xvideos対応？

---
+++ src/main/java/xyz/n7mn/nico_proxy/Xvideos.java
@@ -0,0 +1,58 @@
+package xyz.n7mn.nico_proxy;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import xyz.n7mn.nico_proxy.data.RequestVideoData;
+import xyz.n7mn.nico_proxy.data.ResultVideoData;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Xvideos implements ShareService{
+
+    @Override
+    public ResultVideoData getVideo(RequestVideoData data) throws Exception {
+
+        String url = data.getURL();
+
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        final OkHttpClient client = data.getProxy() != null ? builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+
+        String HtmlText = "";
+        Request request_html = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request_html).execute();
+        if (response.body() != null){
+            HtmlText = response.body().string();
+        }
+        response.close();
+
+        Matcher matcher = Pattern.compile("html5player\\.setVideoUrlHigh\\('(.*)'\\)").matcher(HtmlText);
+        if (matcher.find()){
+            return new ResultVideoData(matcher.group(1), "", false, false, false, "");
+        }
+
+        return null;
+    }
+
+    @Override
+    public ResultVideoData getLive(RequestVideoData data) throws Exception {
+        // なし
+        return null;
+    }
+
+    @Override
+    public String getServiceName() {
+        return "XVIDEOS.com";
+    }
+
+    @Override
+    public String getVersion() {
+        return "20230729";
+    }
+}
