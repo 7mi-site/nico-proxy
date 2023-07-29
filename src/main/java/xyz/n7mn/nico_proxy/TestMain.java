7235b800ce11f309af3a935ed8c6827eb1025748
ちのなみ <tino_nanami@7mi.site>
2023-07-29 11:32:25 +0900

    tiktok対応は諦めた

--- src/main/java/xyz/n7mn/nico_proxy/TestMain.java
+++ src/main/java/xyz/n7mn/nico_proxy/TestMain.java
@@ -1,20 +1,7 @@
 package xyz.n7mn.nico_proxy;
 
-
-import xyz.n7mn.nico_proxy.data.RequestVideoData;
-import xyz.n7mn.nico_proxy.data.ResultVideoData;
-
 public class TestMain {
     public static void main(String[] args)  {
-
-        TikTok tikTok = new TikTok();
-
-        try {
-            ResultVideoData video = tikTok.getVideo(new RequestVideoData("https://www.tiktok.com/@komedascoffee/video/7258220227773746433", null));
-            System.out.println(video.getVideoURL());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }
