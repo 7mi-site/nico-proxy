92b114af79a1f91da7e18a37d8345f0eb73725d2
ちのなみ <tino_nanami@7mi.site>
2023-09-16 18:53:28 +0900

    ツイキャス対応

--- src/main/java/xyz/n7mn/nico_proxy/TestMain.java
+++ src/main/java/xyz/n7mn/nico_proxy/TestMain.java
@@ -9,12 +9,16 @@ public class TestMain {
 
         try{
 
-            ResultVideoData video = new Pornhub().getVideo(new RequestVideoData("https://jp.pornhub.com/view_video.php?viewkey=648be371d8cc2", null));
+            Twicast twicast = new Twicast("1548673979992670209.3fa9fb392b85a7835a04cf6000e426624258e3c1a30f5674704252a23688d77f", "e909327aab8cc9e106e2bf2cbef03f652cfb074958ef159b5aa425300edc5629");
+            // https://twitcasting.tv/twitcasting_jp/movie/776448195
+            // https://twitcasting.tv/hmb_d
+            ResultVideoData live = twicast.getLive(new RequestVideoData("https://twitcasting.tv/hmb_d/movie/776588096", null));
 
-            String videoURL = video.getVideoURL();
-            System.out.println("video url : "+videoURL);
-            System.out.println("title : "+ new Pornhub().getTitle(new RequestVideoData("https://jp.pornhub.com/view_video.php?viewkey=648be371d8cc2", null)));
+            // アーカイブの場合はURL取得できてもリファラが必要
+            System.out.println(live.getVideoURL());
 
+            String title = twicast.getTitle(new RequestVideoData("https://twitcasting.tv/hmb_d/movie/776588096", null));
+            System.out.println(title);
 
         } catch (Exception e){
             e.printStackTrace();
---
+++ src/main/java/xyz/n7mn/nico_proxy/Twicast.java
@@ -0,0 +1,225 @@
+package xyz.n7mn.nico_proxy;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import xyz.n7mn.nico_proxy.data.RequestVideoData;
+import xyz.n7mn.nico_proxy.data.ResultVideoData;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Twicast implements ShareService{
+
+    private final String ClientID;
+    private final String ClientSecret;
+
+    private final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+    public Twicast(String ClientID, String ClientSecret){
+        this.ClientID = ClientID;
+        this.ClientSecret = ClientSecret;
+    }
+
+    @Override
+    public ResultVideoData getVideo(RequestVideoData data) throws Exception {
+        return getLive(data);
+    }
+
+    @Override
+    public ResultVideoData getLive(RequestVideoData data) throws Exception {
+        final OkHttpClient client = data.getProxy() != null ? builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+
+        // https://twitcasting.tv/twitcasting_jp
+
+        String url = "";
+        String jsonText = "";
+
+        Matcher matcher_live = Pattern.compile("https://twitcasting\\.tv/(.+)").matcher(data.getURL());
+        Matcher matcher_live2 = Pattern.compile("https://twitcasting\\.tv/(.+)/movie/(\\d+)").matcher(data.getURL());
+
+
+        String id = "";
+        boolean checkUsers = true;
+
+        if (matcher_live2.find()){
+            id = matcher_live2.group(2);
+            checkUsers = false;
+        }
+
+        if (matcher_live.find() && checkUsers){
+            String[] split = data.getURL().split("/");
+            id = split[split.length - 1];
+            if (id.isEmpty()){
+                id = split[split.length - 2];
+            }
+        }
+
+        if (checkUsers){
+            Request request = new Request.Builder()
+                    .url("https://apiv2.twitcasting.tv/users/"+id)
+                    .addHeader("Accept", "application/json")
+                    .addHeader("X-Api-Version", "2.0")
+                    .addHeader("Authorization", "Basic "+new String(Base64.getEncoder().encode((ClientID+":"+ClientSecret).getBytes(StandardCharsets.UTF_8))))
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (response.body() != null){
+                jsonText = response.body().string();
+            }
+            response.close();
+
+            //System.out.println(jsonText);
+
+            JsonElement json = new Gson().fromJson(jsonText, JsonElement.class);
+            id = String.valueOf(json.getAsJsonObject().getAsJsonObject("user").get("last_movie_id").getAsInt());
+            //System.out.println(id);
+        }
+
+        Request request = new Request.Builder()
+                .url("https://apiv2.twitcasting.tv/movies/"+id)
+                .addHeader("Accept", "application/json")
+                .addHeader("X-Api-Version", "2.0")
+                .addHeader("Authorization", "Basic "+new String(Base64.getEncoder().encode((ClientID+":"+ClientSecret).getBytes(StandardCharsets.UTF_8))))
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response.body() != null){
+            jsonText = response.body().string();
+        }
+        response.close();
+
+        //System.out.println(jsonText);
+
+        JsonElement json = new Gson().fromJson(jsonText, JsonElement.class);
+
+        String htmlText = "";
+        try {
+            if (json.getAsJsonObject().getAsJsonObject("movie").get("hls_url") != JsonNull.INSTANCE){
+                url = json.getAsJsonObject().getAsJsonObject("movie").get("hls_url").getAsString();
+            } else {
+                if (json.getAsJsonObject().getAsJsonObject("movie").get("is_recorded").toString().equals("true")){
+
+                    Request request2 = new Request.Builder()
+                            .url(json.getAsJsonObject().getAsJsonObject("movie").get("link").getAsString())
+                            .build();
+                    Response response2 = client.newCall(request2).execute();
+                    if (response2.body() != null){
+                        htmlText = response2.body().string();
+                    }
+                    response2.close();
+
+                } else {
+                    throw new Exception("Is Not Recorded");
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new Exception("Not Found");
+        }
+
+        if (!htmlText.isEmpty()){
+            Matcher matcher = Pattern.compile("data-movie-playlist='(.+)'").matcher(htmlText);
+            if (matcher.find()){
+                JsonElement json1 = new Gson().fromJson(matcher.group(1), JsonElement.class);
+                //System.out.println(json1.toString());
+                try {
+                    url = json1.getAsJsonObject().get("2").getAsJsonArray().get(0).getAsJsonObject().getAsJsonObject("source").get("url").getAsString();
+                } catch (Exception e){
+                    url = null;
+                }
+            }
+        }
+
+
+        return new ResultVideoData(url, "", true, false, json.getAsJsonObject().getAsJsonObject("movie").get("is_live").toString().equals("true"), "");
+    }
+
+    @Override
+    public String getTitle(RequestVideoData data) throws Exception {
+        final OkHttpClient client = data.getProxy() != null ? builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+        String title = "";
+
+        Matcher matcher_live = Pattern.compile("https://twitcasting\\.tv/(.+)").matcher(data.getURL());
+        Matcher matcher_live2 = Pattern.compile("https://twitcasting\\.tv/(.+)/movie/(\\d+)").matcher(data.getURL());
+
+        boolean isCheckUser = true;
+
+        String id = "";
+
+        if (matcher_live2.find()){
+            id = matcher_live2.group(2);
+            isCheckUser = false;
+        }
+
+        if (matcher_live.find() && isCheckUser){
+            String[] split = data.getURL().split("/");
+            id = split[split.length - 1];
+            if (id.isEmpty()){
+                id = split[split.length - 2];
+            }
+        }
+
+        if (isCheckUser){
+            String jsonText = "";
+            Request request = new Request.Builder()
+                    .url("https://apiv2.twitcasting.tv/users/"+id)
+                    .addHeader("Accept", "application/json")
+                    .addHeader("X-Api-Version", "2.0")
+                    .addHeader("Authorization", "Basic "+new String(Base64.getEncoder().encode((ClientID+":"+ClientSecret).getBytes(StandardCharsets.UTF_8))))
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (response.body() != null){
+                jsonText = response.body().string();
+            }
+            response.close();
+
+            //System.out.println(jsonText);
+
+            JsonElement json = new Gson().fromJson(jsonText, JsonElement.class);
+            id = String.valueOf(json.getAsJsonObject().getAsJsonObject("user").get("last_movie_id").getAsInt());
+            //System.out.println(id);
+        }
+
+        String jsonText = "";
+        Request request = new Request.Builder()
+                .url("https://apiv2.twitcasting.tv/movies/"+id)
+                .addHeader("Accept", "application/json")
+                .addHeader("X-Api-Version", "2.0")
+                .addHeader("Authorization", "Basic "+new String(Base64.getEncoder().encode((ClientID+":"+ClientSecret).getBytes(StandardCharsets.UTF_8))))
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response.body() != null){
+            jsonText = response.body().string();
+        }
+        response.close();
+
+        //System.out.println(jsonText);
+
+        JsonElement json = new Gson().fromJson(jsonText, JsonElement.class);
+        if (json.getAsJsonObject().getAsJsonObject("movie").get("subtitle") != JsonNull.INSTANCE){
+            title = json.getAsJsonObject().getAsJsonObject("movie").get("subtitle").getAsString();
+        } else {
+            title = json.getAsJsonObject().getAsJsonObject("movie").get("title").getAsString();
+        }
+
+        return title;
+    }
+
+    @Override
+    public String getServiceName() {
+        return "ツイキャス";
+    }
+
+    @Override
+    public String getVersion() {
+        return "20230916";
+    }
+}
