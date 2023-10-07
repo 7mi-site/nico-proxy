9f6555b2397f97f222042716928c16dbc177d811
ちのなみ <tino_nanami@7mi.site>
2023-10-07 14:03:18 +0900

    Abema対応(動画のみ)

---
+++ src/main/java/xyz/n7mn/nico_proxy/Abema.java
@@ -0,0 +1,126 @@
+package xyz.n7mn.nico_proxy;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import xyz.n7mn.nico_proxy.data.RequestVideoData;
+import xyz.n7mn.nico_proxy.data.ResultVideoData;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Abema implements ShareService {
+
+
+    @Override
+    public ResultVideoData getVideo(RequestVideoData data) throws Exception {
+        Matcher matcher = Pattern.compile("https://abema.tv/video/episode/(.*)").matcher(data.getURL());
+
+        if (!matcher.find()){
+            throw new Exception("Not Support URL");
+        }
+
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        final OkHttpClient client = data.getProxy() != null ? builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+
+        Request request_html = new Request.Builder()
+                .url(data.getURL())
+                .build();
+        try {
+            Response response = client.newCall(request_html).execute();
+            response.close();
+        } catch (Exception e){
+            throw e;
+        }
+
+        Request request_api = new Request.Builder()
+                .url("https://api.p-c3-e.abema-tv.com/v1/video/programs/"+matcher.group(1)+"?division=0&include=tvod")
+                .addHeader("Authorization","bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiI3YWQ5NjQ1Ni0zZjFmLTRiYTctOTQ1OC1jOTA0MzQyYTNiNDMiLCJleHAiOjIxNDc0ODM2NDcsImlzcyI6ImFiZW1hLmlvL3YxIiwic3ViIjoiOTRjeXh3UGR5OVdHcHcifQ.Muv9eT4Tmy4JsSOGTVexwxuGnf2ZkwL1RkBo6MrSZGg")
+                .addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0")
+                .addHeader("Referer", "https://abema.tv/")
+                .build();
+
+       String api_result = "";
+        try {
+            Response response = client.newCall(request_api).execute();
+            api_result = response.body().string();
+            response.close();
+        } catch (Exception e){
+            throw e;
+        }
+
+        //System.out.println(api_result);
+
+        JsonElement json = new Gson().fromJson(api_result, JsonElement.class);
+        String string = json.getAsJsonObject().getAsJsonObject("playback").get("hlsPreview").getAsString();
+
+        return new ResultVideoData(string, "", true, false, false, null);
+    }
+
+    @Override
+    public ResultVideoData getLive(RequestVideoData data) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String getTitle(RequestVideoData data) throws Exception {
+        Matcher matcher = Pattern.compile("https://abema.tv/video/episode/(.*)").matcher(data.getURL());
+
+        if (!matcher.find()){
+            throw new Exception("Not Support URL");
+        }
+
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        final OkHttpClient client = data.getProxy() != null ? builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+
+        Request request_html = new Request.Builder()
+                .url(data.getURL())
+                .build();
+        try {
+            Response response = client.newCall(request_html).execute();
+            response.close();
+        } catch (Exception e){
+            throw e;
+        }
+
+        Request request_api = new Request.Builder()
+                .url("https://api.p-c3-e.abema-tv.com/v1/video/programs/"+matcher.group(1)+"?division=0&include=tvod")
+                .addHeader("Authorization","bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiI3YWQ5NjQ1Ni0zZjFmLTRiYTctOTQ1OC1jOTA0MzQyYTNiNDMiLCJleHAiOjIxNDc0ODM2NDcsImlzcyI6ImFiZW1hLmlvL3YxIiwic3ViIjoiOTRjeXh3UGR5OVdHcHcifQ.Muv9eT4Tmy4JsSOGTVexwxuGnf2ZkwL1RkBo6MrSZGg")
+                .addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0")
+                .addHeader("Referer", "https://abema.tv/")
+                .build();
+
+        String api_result = "";
+        try {
+            Response response = client.newCall(request_api).execute();
+            api_result = response.body().string();
+            response.close();
+        } catch (Exception e){
+            throw e;
+        }
+
+        //System.out.println(api_result);
+
+        JsonElement json = new Gson().fromJson(api_result, JsonElement.class);
+
+        if (!json.getAsJsonObject().getAsJsonObject("series").get("title").isJsonNull()){
+            return json.getAsJsonObject().getAsJsonObject("series").get("title").getAsString();
+        }
+
+        return "";
+    }
+
+    @Override
+    public String getServiceName() {
+        return "Abema";
+    }
+
+    @Override
+    public String getVersion() {
+        return "20231007";
+    }
+}
