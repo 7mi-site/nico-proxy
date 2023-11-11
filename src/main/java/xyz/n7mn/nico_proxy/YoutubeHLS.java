ca0a16131d1b2af9a73498bc55a5d3eeb2554b1d
ちのなみ <tino_nanami@7mi.site>
2023-11-11 13:15:33 +0900

    Youtube: Liveでも共通処理だったのとタイトル取得できるように

--- src/main/java/xyz/n7mn/nico_proxy/YoutubeHLS.java
+++ src/main/java/xyz/n7mn/nico_proxy/YoutubeHLS.java
@@ -21,7 +21,7 @@ public class YoutubeHLS implements ShareService{
     @Override
     public ResultVideoData getVideo(RequestVideoData data) throws Exception {
 
-        String url = data.getURL();
+        String url = data.getURL().split("&")[0];
         Matcher matcher = Pattern.compile("(youtu\\.be/|youtube.com/watch\\?v=)(.*)").matcher(url);
 
         if (!matcher.find()){
@@ -160,17 +160,79 @@ public class YoutubeHLS implements ShareService{
             //e.printStackTrace();
         }
 
-        return new ResultVideoData(VideoURL, AudioURL, true, false, false, "", caption);
+        return new ResultVideoData(VideoURL, AudioURL, true, false, json.getAsJsonObject().getAsJsonObject("playabilityStatus").get("liveStreamability") != null, "", caption);
     }
 
     @Override
     public ResultVideoData getLive(RequestVideoData data) throws Exception {
-        return null;
+        return getVideo(data);
     }
 
     @Override
     public String getTitle(RequestVideoData data) throws Exception {
-        return null;
+
+        String url = data.getURL().split("&")[0];
+        Matcher matcher = Pattern.compile("(youtu\\.be/|youtube.com/watch\\?v=)(.*)").matcher(url);
+
+        if (!matcher.find()){
+            throw new Exception("Not Support URL");
+        }
+
+        String videoID = matcher.group(2);
+
+        final OkHttpClient client = data.getProxy() != null ? new OkHttpClient.Builder().proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+
+
+        String SendJson = "{\"context\": {\"client\": {\"clientName\": \"IOS\", \"clientVersion\": \"17.33.2\", \"deviceModel\": \"iPhone14,3\", \"userAgent\": \"com.google.ios.youtube/17.33.2 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)\", \"hl\": \"ja\", \"timeZone\": \"UTC\", \"utcOffsetMinutes\": 0}}, \"videoId\": \""+videoID+"\", \"playbackContext\": {\"contentPlaybackContext\": {\"html5Preference\": \"HTML5_PREF_WANTS\"}}, \"contentCheckOk\": true, \"racyCheckOk\": true}";
+
+        final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(SendJson, JSON);
+
+        Request request_iosAPI = new Request.Builder()
+                .url("https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false")
+                .addHeader("Cookie", "PREF=hl=en&tz=UTC; SOCS=CAI; GPS=1; YSC=cfiTHKBLEmo; VISITOR_INFO1_LIVE=P3aYDmsojgI; VISITOR_PRIVACY_METADATA=CgJKUBICGgA%3D")
+                .addHeader("User-Agent", "com.google.ios.youtube/17.33.2 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)")
+                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+                .addHeader("Accept-Language", "en-us,en;q=0.5")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("X-Youtube-Client-Name", "5")
+                .addHeader("X-Youtube-Client-Version", "17.33.2")
+                .addHeader("Origin", "https://www.youtube.com")
+                .addHeader("Accept-Encoding", "gzip")
+                .addHeader("Connection", "close")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request_iosAPI).execute();
+
+        String ResponseJson = "";
+        if (response.body() != null){
+
+            //Headers headers = response.headers();
+            //System.out.println(response.code());
+            //for (String name : headers.names()) {
+            //    System.out.println(name + " : " + headers.get(name));
+            //}
+
+            ByteArrayOutputStream decompressBaos = new ByteArrayOutputStream();
+            try (InputStream gzip = new GZIPInputStream(new ByteArrayInputStream(response.body().bytes()))) {
+                int b;
+                while ((b = gzip.read()) != -1) {
+                    decompressBaos.write(b);
+                }
+            }
+            byte[] byteArray = decompressBaos.toByteArray();
+            ResponseJson = new String(byteArray, StandardCharsets.UTF_8);
+            //System.out.println(ResponseJson);
+
+            response.close();
+        }
+
+        //System.out.println(ResponseJson);
+
+        JsonElement json = new Gson().fromJson(ResponseJson, JsonElement.class);
+
+        return json.getAsJsonObject().getAsJsonObject("videoDetails").get("title").getAsString();
     }
 
     @Override
