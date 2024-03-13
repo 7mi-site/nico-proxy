b2ea4cce9f937d9d487240ab3ac2794d6ba2a016
ちのなみ <tino_nanami@7mi.site>
2024-03-13 22:14:46 +0900

    Gimy対応

--- src/main/java/xyz/n7mn/nico_proxy/Gimy.java
+++ src/main/java/xyz/n7mn/nico_proxy/Gimy.java
@@ -63,7 +63,41 @@ public class Gimy implements ShareService{
 
     @Override
     public String getTitle(RequestVideoData data) throws Exception {
-        return null;
+        Matcher matcher = Pattern.compile("https://gimy\\.ai/eps/(.+)\\.html").matcher(data.getURL());
+        if (!matcher.find()){
+            throw new Exception("gimy.ai Not Support URL");
+        }
+
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        final OkHttpClient client = data.getProxy() != null ? builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(data.getProxy().getProxyIP(), data.getProxy().getPort()))).build() : new OkHttpClient();
+
+        String HtmlText = "";
+        try {
+            Request request_html = new Request.Builder()
+                    .url(data.getURL())
+                    .build();
+            Response response = client.newCall(request_html).execute();
+            if (response.body() != null){
+                HtmlText = response.body().string();
+            }
+            response.close();
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+
+        //System.out.println(HtmlText);
+
+        Matcher json_matcher = Pattern.compile("player_data=\\{(.+)\\}").matcher(HtmlText);
+        if (!json_matcher.find()){
+            throw new Exception("gimy.ai Not Found");
+        }
+
+        final String json = "{"+json_matcher.group(1)+"}";
+        //System.out.println(json);
+        JsonElement JsonData = new Gson().fromJson(json, JsonElement.class);
+        if (JsonData.getAsJsonObject().get("vod_data").getAsJsonObject().has("vod_name")){
+            return JsonData.getAsJsonObject().get("vod_data").getAsJsonObject().get("vod_name").getAsString();
+        }
     }
 
     @Override
