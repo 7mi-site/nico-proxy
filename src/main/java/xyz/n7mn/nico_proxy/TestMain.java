ab7e335838fab6cd13e98f64650771f3890dd71b
ちのなみ <tino_nanami@7mi.site>
2024-05-02 23:12:19 +0900

    piaproに対応

--- src/main/java/xyz/n7mn/nico_proxy/TestMain.java
+++ src/main/java/xyz/n7mn/nico_proxy/TestMain.java
@@ -1,21 +1,17 @@
 package xyz.n7mn.nico_proxy;
 
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import xyz.n7mn.nico_proxy.data.ProxyData;
 import xyz.n7mn.nico_proxy.data.RequestVideoData;
 import xyz.n7mn.nico_proxy.data.ResultVideoData;
 
-import java.io.IOException;
 
 
 public class TestMain {
 
     public static void main(String[] args) throws Exception {
-        String video = new Abema().getTitle(new RequestVideoData("https://abema.tv/channels/fighting-sports/slots/EEPqpkB2qEdYgX", null));
-        System.out.println(video);
+        ResultVideoData video = new Piapro().getVideo(new RequestVideoData("https://piapro.jp/t/KQn0", null));
+        //System.out.println(video);
+        System.out.println(new Piapro().getTitle(new RequestVideoData("https://piapro.jp/t/KQn0", null)));
 
     }
 }
