110f683a43f1e90cd8643eb0e231f598d638e7a9
ちのなみ <tino_nanami@7mi.site>
2024-06-26 21:27:56 +0900

    整理

--- src/main/java/xyz/n7mn/nico_proxy/TestMain.java
+++ src/main/java/xyz/n7mn/nico_proxy/TestMain.java
@@ -1,20 +1,8 @@
 package xyz.n7mn.nico_proxy;
 
-
-import xyz.n7mn.nico_proxy.data.ProxyData;
-import xyz.n7mn.nico_proxy.data.RequestVideoData;
-import xyz.n7mn.nico_proxy.data.ResultVideoData;
-
-
-
 public class TestMain {
 
     public static void main(String[] args) throws Exception {
-
-        ResultVideoData live = new Vimeo().getVideo(new RequestVideoData("https://vimeo.com/944027008?share=copy", null));
-        //String title = new Vimeo().getTitle(new RequestVideoData("https://vimeo.com/944027008?share=copy", null));
-        //System.out.println(title);
-        System.out.println(live.getVideoURL());
 
     }
 }
