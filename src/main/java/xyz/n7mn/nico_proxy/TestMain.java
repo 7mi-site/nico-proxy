c6b0a9a50a141b350576cab11f1d7df2d33d1a40
Chino Nanami <tino_nanami@7mi.site>
2024-09-17 21:06:02 +0900

    bilibili.comの見れないのを修正

--- src/main/java/xyz/n7mn/nico_proxy/TestMain.java
+++ src/main/java/xyz/n7mn/nico_proxy/TestMain.java
@@ -8,7 +8,5 @@ public class TestMain {
 
     public static void main(String[] args) throws Exception {
 
-        new BiliBiliCom_fromAPI().getVideo(new RequestVideoData("https://www.bilibili.com/video/BV1Lx421279E/", null));
-
     }
 }
