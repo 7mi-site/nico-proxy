a9c734cb60cd65c87d094f98b7417f8b41af09f3
Chino Nanami <tino_nanami@7mi.site>
2024-10-12 15:51:33 +0900

    up

--- src/main/java/xyz/n7mn/nico_proxy/Constant.java
+++ src/main/java/xyz/n7mn/nico_proxy/Constant.java
@@ -2,7 +2,7 @@ package xyz.n7mn.nico_proxy;
 
 public class Constant {
 
-    public static String nico_proxy_Version = "1.22.4";
-    public static String nico_proxy_UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0 nico-proxy/"+nico_proxy_Version;
+    public static String nico_proxy_Version = "1.22.5";
+    public static String nico_proxy_UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0 nico-proxy/"+nico_proxy_Version;
 
 }
