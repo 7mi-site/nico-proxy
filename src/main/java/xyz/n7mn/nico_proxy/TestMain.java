6f633b13f5a86e1ec91be5d1df2ed43e6292ebbb
Chino Nanami <tino_nanami@7mi.site>
2025-01-29 19:35:55 +0900

    bugfix

--- src/main/java/xyz/n7mn/nico_proxy/TestMain.java
+++ src/main/java/xyz/n7mn/nico_proxy/TestMain.java
@@ -1,15 +1,9 @@
 package xyz.n7mn.nico_proxy;
 
-import xyz.n7mn.nico_proxy.data.ProxyData;
-import xyz.n7mn.nico_proxy.data.RequestVideoData;
-import xyz.n7mn.nico_proxy.data.ResultVideoData;
-
 public class TestMain {
 
     public static void main(String[] args) throws Exception {
 
-        ResultVideoData video = new TVer().getLive(new RequestVideoData("https://tver.jp/live/cx", null));
-        System.out.println(video.getVideoURL());
 
     }
 }
